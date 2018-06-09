5cfddc532372206ff55588f449a30ace82c4e12c
Nicholas Ray <nrayemail@gmail.com>
2018-06-09 13:44:34 -0600

    Add others

--- go.sh
+++ go.sh
@@ -1,1 +1,5 @@
+# Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Show hidden files in mac
+defaults write com.apple.finder AppleShowAllFiles NO
