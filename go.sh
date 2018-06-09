c6b30905a57d56d0aacf2d4afa2642c4be853b48
Nicholas Ray <nray@Nicholass-MacBook-Pro-2.local>
2018-06-09 15:41:11 -0600

    Make more edits

--- go.sh
+++ go.sh
@@ -1,5 +1,37 @@
-# Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#!/bin/sh
+
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\n$fmt\n" "$@"
+}
+
+fancy_echo "Sit back and relax..."
 
 # Show hidden files in mac
-defaults write com.apple.finder AppleShowAllFiles NO
+fancy_echo "Enable apple finder to show hidden files..."
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# Install brew
+if ! command -v brew >/dev/null; then
+  fancy_echo "Installing homebrew..."
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Install Brew Cask to make app installation easy
+fancy_echo "Installing cask..."
+brew tap homebrew/cask
+
+brew install zsh
+brew install tmux
+brew install git
+brew install openssl
+brew install neovim
+brew install postgresql
+brew install mysql
+brew install chruby
+brew install ruby-install
+
+fancy_echo "Installing Ruby 2.5.1..."
+ruby-install ruby-2.5.1
