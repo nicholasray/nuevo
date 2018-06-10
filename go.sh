65098a98b64e0f93bdac01ada573d7c1ac0674eb
Nicholas Ray <nray@Nicholass-MacBook-Pro-2.local>
2018-06-09 20:58:09 -0600

    Add more stuff

--- go.sh
+++ go.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+trap cleanup INT
+
+set -e
+
+function cleanup() {
+  fancy_echo "Exiting..."
+  exit 1
+}
 
 fancy_echo() {
   local fmt="$1"; shift
@@ -8,6 +19,13 @@ fancy_echo() {
 }
 
 fancy_echo "Sit back and relax..."
+
+mkdir -p "$HOME/Development"
+
+if [ ! -d  "$HOME/dotfiles" ]; then
+  fancy_echo "Cloning dot_files..."
+  git clone git@github.com:nicholasray/dotfiles.git "$HOME/dotfiles"
+fi
 
 # Show hidden files in mac
 fancy_echo "Enable apple finder to show hidden files..."
@@ -19,26 +37,49 @@ if ! command -v brew >/dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+function install_pkg_if_absent() {
+  local pkg="$1"
+
+  fancy_echo "Checking if $pkg exists..."
+  if ! brew ls --versions $pkg > /dev/null 2>&1; then
+    fancy_echo "Installing $1..."
+    brew install "$1"
+  fi
+}
+
 # Install Terminal Stuff
-brew install zsh
-brew install tmux
-brew install git
-brew install openssl
-brew install neovim
-brew install postgresql
-brew install mysql
-brew install chruby
-brew install ruby-install
-brew install redis
+install_pkg_if_absent zsh
+install_pkg_if_absent tmux
+install_pkg_if_absent git
+install_pkg_if_absent openssl
+install_pkg_if_absent neovim
+install_pkg_if_absent postgresql
+install_pkg_if_absent mysql
+install_pkg_if_absent chruby
+install_pkg_if_absent ruby-install
+install_pkg_if_absent redis
 
 # Install Brew Cask to make app installation easy
-fancy_echo "Installing cask..."
+fancy_echo "Tapping cask..."
 brew tap homebrew/cask
-brew cask install google-chrome
-brew cask install spectacle
-brew cask install docker
-brew cask install firefox
-brew cask install sourcetree
+
+function install_app_if_absent() {
+  local pkg="$1"
+
+  fancy_echo "Checking if $pkg exists..."
+  if ! brew cask ls --versions $pkg > /dev/null 2>&1; then
+    fancy_echo "Installing $1..."
+    brew cask install --force "$1"
+  fi
+}
+
+install_app_if_absent google-chrome
+install_app_if_absent spectacle
+install_app_if_absent docker
+install_app_if_absent firefox
+install_app_if_absent sourcetree
+install_app_if_absent iterm2
+install_app_if_absent slack
 
 # Install recent ruby version
 
@@ -46,3 +87,6 @@ if [ ! -d "$HOME/.rubies/ruby-2.5.1" ]; then
   fancy_echo "Installing Ruby 2.5.1..."
   ruby-install ruby-2.5.1
 fi
+
+fancy_echo "SUCCESS! Enjoy your new setup!"
+exit 0
