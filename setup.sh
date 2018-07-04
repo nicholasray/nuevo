7862e537ec7e79e4361baf658ba8082c2cd5229c
Nick Ray <nray@nray.io>
2018-07-04 01:00:31 -0600

    Add more

--- setup.sh
+++ setup.sh
@@ -171,6 +171,7 @@ install_app_if_absent vagrant
 install_app_if_absent java8
 install_app_if_absent adobe-acrobat-reader
 install_app_if_absent 1password
+install_app_if_absent sequel-pro
 
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby 2.5.1
